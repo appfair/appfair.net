2822e5d99f668f701524680344003655997e6a14
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-11-16 23:26:39 -0500

    Update translations

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -23,10 +23,40 @@ export const ui = {
         'fr': 'Tous droits réservés',
         'es': 'Todos los derechos reservados',
     },
+    'footer.links': {
+        'en': 'Links',
+        'fr': 'Liens',
+        'es': 'Enlaces',
+    },
+    'footer.browse': {
+        'en': 'Browse Apps',
+        'fr': 'Parcourir les applications',
+        'es': 'Explorar aplicaciones',
+    },
+    'footer.legal': {
+        'en': 'Legal',
+        'fr': 'Mentions légales',
+        'es': 'Legal',
+    },
+    'footer.privacy': {
+        'en': 'Privacy Policy',
+        'fr': 'Politique de confidentialité',
+        'es': 'Política de privacidad',
+    },
     'language.select': {
         'en': 'Select language',
         'fr': 'Choisir la langue',
         'es': 'Seleccionar idioma',
+    },
+    'index.header': {
+        'en': 'A list of all the apps currently available through the App Fair Project',
+        'fr': 'Une liste de toutes les applications actuellement disponibles via le projet App Fair',
+        'es': 'Una lista de todas las aplicaciones actualmente disponibles a través del Proyecto App Fair',
+    },
+    'index.title': {
+        'en': 'The App Fair Apps',
+        'fr': 'Les applications App Fair',
+        'es': 'Las aplicaciones de App Fair',
     }
 };
 
